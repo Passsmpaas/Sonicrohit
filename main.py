0c0f67e578bb3f24a7a7da0fefb7a471b660e043
Passsmpaas <lokeshsingh17628@gmail.com>
2025-08-14 15:01:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ bot = Client(
 
 
 #port
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+port = os.environ.get("PORT", 8080)
+app.run(host="0.0.0.0", port=8080)
 
-
+#commands
 @bot.on_message(filters.command(["start"]))
 async def start(bot: Client, m: Message):
     editable = await m.reply_text(
